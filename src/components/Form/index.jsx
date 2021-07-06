922039578edb77508a45c8cc2e40fcb7576633f6
ThiagoMendesDev <mendes@convertte.com.br>
2021-07-06 17:09:15 -0300

    form-testing

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -11,12 +11,7 @@ export default function(){
                     <p>
                         <label>Your Email: <input type="email" name="email" /></label>
                     </p>
-                    <p>
-                        <label>Your Role: <select name="role[]" multiple>
-                        <option value="leader">Leader</option>
-                        <option value="follower">Follower</option>
-                        </select></label>
-                    </p>
+                    <input type="hidden" name="form-name" value="contact"></input>
                     <p>
                         <label>Message: <textarea name="message"></textarea></label>
                     </p>
