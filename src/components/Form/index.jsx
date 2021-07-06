234f660032023bb15fdad40c0ee6fc6e9a6c0aea
ThiagoMendesDev <mendes@convertte.com.br>
2021-07-06 16:36:48 -0300

    form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,15 +4,15 @@ import { Form } from './style'
 export default function(){
     return(
         <Form>
-            <form action="">
+            <form action="" netlify>
                 <h3>Nome completo</h3>
-                <input type="text" placeholder="Nome completo"/>
+                <input name="nome" type="text" placeholder="Nome completo"/>
                 <h3>Email</h3>
-                <input type="email" placeholder="Email"/>
+                <input name="email" type="email" placeholder="Email"/>
                 <h3>Telefone/whatsapp</h3>
-                <input type="text" placeholder="Telefone/whatsapp" /><br/>
+                <input name="telefone" type="text" placeholder="Telefone/whatsapp" /><br/>
                 <h3>Mensagem</h3>
-                <textarea cols="30" rows="10" placeholder="Digite Sua Mensagem"></textarea>
+                <textarea name="mensagem" cols="30" rows="10" placeholder="Digite Sua Mensagem"></textarea>
                 <input type="submit" value="Enviar" />
             </form>
         </Form>
