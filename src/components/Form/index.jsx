c4caa5225a34263bb4c05dd20d3917ec6aea7fe2
ThiagoMendesDev <mendes@convertte.com.br>
2021-07-06 16:52:48 -0300

    teste

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -9,10 +9,10 @@ export default function(){
                 <input name="nome" type="text" placeholder="Nome completo"/>
                 <h3>Email</h3>
                 <input name="email" type="email" placeholder="Email"/>
-                <h3>Telefone/whatsapp</h3>
-                <input name="telefone" type="text" placeholder="Telefone/whatsapp" /><br/>
+                {/* <h3>Telefone/whatsapp</h3>
+                <input name="telefone" type="text" placeholder="Telefone/whatsapp" /><br/> */}
                 <h3>Mensagem</h3>
-                <textarea name="mensagem" cols="30" rows="10" placeholder="Digite Sua Mensagem"></textarea>
+                <textarea name="message" cols="30" rows="10" placeholder="Digite Sua Mensagem"></textarea>
                 <input type="submit" value="Enviar" />
             </form>
         </Form>
